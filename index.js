befa4d1edd17791046436b1b6e78400546dd05f8
t3rr0r <mail@t3rr0r.com>
2015-07-04 17:09:55 -0400

    accept cross origin requests

--- index.js
+++ index.js
@@ -57,6 +57,18 @@ router.get('/posts/trending', function(req, res) {
 
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if ('OPTIONS' === req.method || '/health_check' === req.path) {
+	res.sendStatus(200);
+    } else {
+	next();
+    }
+});
+
 app.use(compression());
 app.use('/api', router);
 
